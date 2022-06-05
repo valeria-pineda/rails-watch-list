854cc7fc12094d922c0930f5d1751f6312966b4d
Valeria Pineda <valerializette.pinedacruz@kedgebs.com>
2022-06-05 15:33:52 +0200

    First commit

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,35 @@
+class BookmarksController < ApplicationController
+  before_action :find_list, only: [ :new, :create ]
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @movie = Movie.find(params[:bookmark][:movie_id])
+    @bookmark.list = @list
+    @bookmark.movie = @movie
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
+  end
+
+  private
+
+  def find_list
+    @list = List.find(params[:list_id])
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id, :list)
+  end
+end
